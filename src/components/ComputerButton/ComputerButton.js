2acf75cc296ad2f8b49ea1e3f2252b8c1510e862
Melliams <dawid@adesso-mobile.de>
2022-02-11 10:57:56 +0100

    If-Abfrage, wann man gewonen/verloren hat (unvollständig)

--- src/components/ComputerButton/ComputerButton.js
+++ src/components/ComputerButton/ComputerButton.js
@@ -3,32 +3,33 @@ import "./ComputerButton.css";
 
 import ReactDOM from "react-dom";
 
-function ComputerButtons({computerButton}){
+function ComputerButtons({computerButton}) {
 
     const [currentButton, setCurrentButton] = useState(computerButton);
 
     //use Effekt= wenn state sich ändert, wenn [] macht er nur 1x, wenn man was reinschreibt macht er es bei jedem mal wenn sich was ändert
     useEffect(() => {
         setCurrentButton(computerButton) // Update the document title using the browser API
-    },[computerButton]); //macht es jedes mal
+    }, [computerButton]); //macht es jedes mal
 
-        return (
-            <div>
-                <div className="container-buttons">
-                    <div className="Computer">
-                        <p className="p">Der Computer spielt:  {currentButton?.title}</p>
-                        <div className="currenticon">{currentButton?.icon}</div>
-                        <div className="currenttitle">{currentButton?.title}</div>
-
-                    </div>
+    return (
+        <div>
+            <div className="container-buttons">
+                <div className="Computer">
+                    <p className="p">Der Computer spielt: {currentButton?.title}</p>
+                    <div className="currenticon">{currentButton?.icon}</div>
+                    <div className="currenttitle">{currentButton?.title}</div>
 
                 </div>
+
             </div>
-        );
-    }
-    const rootElement=
-        document.getElementById("root");
-    ReactDOM.render(<ComputerButtons/>, rootElement);
+        </div>
+    );
+}
+
+const rootElement =
+    document.getElementById("root");
+ReactDOM.render(<ComputerButtons/>, rootElement);
 
 
 export default ComputerButtons;
